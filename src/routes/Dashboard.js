1530ad0f7a1189d9905a68cfb0761aab6f5793ec
eunshinNoh <nes0410@gmail.com>
2023-01-15 21:34:28 -0700

    refs #4 [Dashboard] save datas to localstorage

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -8,15 +8,21 @@ import Button from '../componenents/Button'
 
 function Dashboard() {
   /** data */
-  const [joke, setJoke] = useState("")
+  const [joke, setJoke] = useState('')
+  // jokes: when localStorage has jokes(key) and localStorage jokes array(value) is not empty, localStorage jokes array is used. if not default empty array. 
+  const [jokes, setJokes] = useState(localStorage.jokes && JSON.parse(localStorage.jokes).length !== 0 ? JSON.parse(localStorage.getItem("jokes")) : [])
   /** call api */
   const { loading, data, error, refetch } = useAxios({ url: "https://icanhazdadjoke.com/", headers: { Accept: "application/json" } })
-  console.log(`Loading: ${loading}\nData: ${data&&data.data&&data.data.joke})}\nError: ${error}`)
+  console.log(`Loading: ${loading}\nData: ${data && data.data && data.data.joke}\nError: ${error}`)
+  
   useEffect(() => {
     if (data&&data.data) {
-        setJoke(data.data.joke)  
+      setJoke(data.data.joke) //update joke
+      setJokes([...jokes, data.data.joke])  //add joke to jokes array
     }
   }, [data])
+  localStorage.setItem("jokes", JSON.stringify(jokes))  //save jokes array data to localStorage
+
   return (
     <div className={styles.container}>
       <div>
