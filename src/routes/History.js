3bd08b85fbb959f3c238275b0a8e0d42c51e02f1
eunshinNoh <nes0410@gmail.com>
2023-01-15 21:37:06 -0700

    refs #5 [History] implement basic page

--- src/routes/History.js
+++ src/routes/History.js
@@ -1,11 +1,14 @@
+import {useState} from 'react'
 // css
 import styles from '../style/history.module.css'
 
 function History() {
+  // jokes: when localStorage has jokes(key) and localStorage jokes array(value) is not empty, localStorage jokes array is used. if not default empty array. 
+  const [history, setHistory] = useState(localStorage.jokes && JSON.parse(localStorage.jokes).length !== 0 ? JSON.parse(localStorage.getItem("jokes")) : [])
   return (
     <div className={styles.container}>
       <div>
-        History
+        {history.map((joke, index) => <h2 key={index}>{joke}</h2>)}
       </div>
     </div>
   );
