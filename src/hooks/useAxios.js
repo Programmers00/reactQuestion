466b23f051eecff55e6cac45c1425d12f2fc79b3
eunshinNoh <nes0410@gmail.com>
2023-01-17 12:44:15 -0700

    refs #3 useAxios, comment

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,19 +2,23 @@ import {useState, useEffect} from 'react'
 import defaultAxios from "axios";
 /** hook useAxios wrap up the axios: parameter => options(url, header), axiosInstance*/
 export const useAxios = (options, axiosInstance = defaultAxios) => {
+  // state for loading, data, error
   const [state, setState] = useState({
     loading: true,
     data: null,
     error: null
   })
+  // trigger for refetch
   const [trigger, setTrigger] = useState(0)
+  /** refetch: re-call  */
   const refetch = () => {
     setState({
       ...state,
       loading: true
     })
-    setTrigger(Date.now())
+    setTrigger(Date.now()) // trigger!
   }
+  /** call api when trigger */
   useEffect(() => {
     axiosInstance(options).then(data => {
       console.log("##success")
