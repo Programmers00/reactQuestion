1c23c97b5e9bbd992ac5fe04cd4a5d03a68b883e
eunshinNoh <nes0410@gmail.com>
2023-01-17 00:01:51 -0700

    refs #8 add clear function

--- src/componenents/Input.js
+++ src/componenents/Input.js
@@ -4,19 +4,22 @@ import styles from '../style/input.module.css'
 // hook
 import { useInput } from '../hooks/useInput'
 /** Input componentn: parameter => placeholder, type, validator, callback  */
-function Input({placeholder = "please", type = "text", validator = ()=>{}, callback}) {
+function Input({placeholder = "please", type = "text", validator = ()=>{}, callback, trigger}) {
   // useInput
   const input = useInput("", validator)
   // value from input
-  const { value } = input
-  
+  const { value, setValue } = input
   // observe value
   useEffect(() => {
     typeof callback === 'function' && callback(value)
   }, [value])
+  // trigger: if true, clear input
+  useEffect(() => {
+    if(trigger === true) setValue("")
+  }, [trigger])
   return (
     <div className={styles.container}>
-      {type === "textArea" ? <textarea placeholder={placeholder}{...input}/> : <input placeholder={placeholder}{...input} />}
+      {type === "textArea" ? <textarea placeholder={placeholder} value={input.value} onChange={input.onChange} /> : <input placeholder={placeholder} value={input.value} onChange={input.onChange} />}
     </div>
   );
 }
--- src/routes/AddJoke.js
+++ src/routes/AddJoke.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 // css
 import styles from '../style/addJoke.module.css'
 // componenents
@@ -7,26 +7,34 @@ import Input from '../componenents/Input'
 
 function AddJoke() {
   /** data */
-  // joke: 
+  // joke
   const [newJoke, setNewJoke] = useState('')
   // jokes: when localStorage has jokes(key) and localStorage jokes array(value) is not empty, localStorage jokes array is used. if not default empty array. 
   const [jokes, setJokes] = useState(localStorage.jokes && JSON.parse(localStorage.jokes).length !== 0 ? JSON.parse(localStorage.getItem("jokes")) : [])
+  // trigger clear
+  const [clear, setClear] = useState(false)
   /** function: check validation*/
   const maxLengthValidator = (value) => value.length <= 100
   /** function: observe joke in inputbox */
   const observeJoke = (jokeData) => {
+    setClear(false)
     setNewJoke(jokeData)
   }
   /** function: add new joke */
   const addJoke = () => {
     newJoke.length !== 0 && setJokes([...jokes, newJoke])
+    setNewJoke("")
+    setClear(true)
   }
+  useEffect(() => {
+    console.log("##clear", clear)
+  }, [clear])
   // save jokes array data to localStorage
   localStorage.setItem("jokes", JSON.stringify(jokes))
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <Input type="textArea" placeholder="Please Type A New Joke" validator={maxLengthValidator} callback={observeJoke}></Input>
+        <Input type="textArea" placeholder="Please Type A New Joke" validator={maxLengthValidator} callback={observeJoke} trigger={clear}></Input>
       </div>
       <div className={styles.buttonContainer}>
         <Button title="Add A New Joke" onClick={addJoke} style={{ width: '50vw'}}></Button>
